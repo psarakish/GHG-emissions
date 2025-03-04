28bf5addc1a00a9424d83035ae29b4d4d4f55f3d
Harry Psarakis <psarakish@gmail.com>
2025-03-04 12:47:55 +0200

    Update and rename 1Chart.ipynb to 1Chart.py

---
+++ 1Chart.py
@@ -0,0 +1,7 @@
+#Libraties
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Load Data
+data = pd.read_excel("EDGAR_2024_GHG_booklet_2024.xlsx", sheet_name="GHG_totals_by_country")
